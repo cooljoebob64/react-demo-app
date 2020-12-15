703280c034ce4293d6f94c16e3206e1b11a564c4
Joshua Luppes <cooljoebob64@gmail.com>
2020-12-15 11:05:10 -0600

    Initial user commit. Functioning React app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent'
+import FunFact from './FunFact';
+import MyName from './MyName';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The new app is here!</h1>
+      <h2>Here is some stuff</h2>
+
+      <MyComponent></MyComponent>
+      <MyName></MyName>
+      <FunFact></FunFact>
     </div>
   );
 }
---
+++ src/FunFact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FunFact = () => {
+  return <div>Here is a fun fact!</div>;
+};
+
+export default FunFact;
